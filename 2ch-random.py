a2482ee77f40770103e83af940d64c15521c3676
Haargrad <61315483+Haargrad@users.noreply.github.com>
2023-04-16 14:52:14 +0300

    Update 2ch-random.py

--- 2ch-random.py
+++ 2ch-random.py
@@ -4,10 +4,12 @@ import tempfile
 import requests
 import youtube_dl
 import datetime
+import json
 from datetime import timedelta
 from telegram import Update, InputMediaVideo
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
+SUBSCRIBERS_FILE = "subscribers.json"
 TELEGRAM_TOKEN = ''
 
 subscribed_chats = {}
@@ -26,7 +28,7 @@ def is_time_in_range(start, end):
 def schedule_random_video(context: CallbackContext):
     start_time = "08:00:00"
     end_time = "21:00:00"
-    
+
     if is_time_in_range(start_time, end_time):
         now = datetime.datetime.now()
         for chat_id, interval in subscribed_chats.items():
@@ -67,7 +69,7 @@ def get_random_2hk_video_url():
                 return f'https://2ch.hk{video_path}'
 
     return None
-    
+
 def send_video(chat_id, context: CallbackContext):
     video_url = get_random_2hk_video_url()
     if video_url:
@@ -88,14 +90,53 @@ def send_video(chat_id, context: CallbackContext):
     else:
         context.bot.send_message(chat_id=chat_id, text='Не удалось получить видео. Попробуйте еще раз.')
 
+def parse_interval(interval_str):
+    if ':' in interval_str:
+        time_parts = interval_str.split(':')
+        hours, minutes, seconds = int(time_parts[0]), int(time_parts[1]), int(time_parts[2])
+        return timedelta(hours=hours, minutes=minutes, seconds=seconds)
+    elif interval_str[-1].lower() == 'm':
+        return timedelta(minutes=int(interval_str[:-1]))
+    elif interval_str[-1].lower() == 'h':
+        return timedelta(hours=int(interval_str[:-1]))
+    elif interval_str[-1].lower() == 'd':
+        return timedelta(days=int(interval_str[:-1]))
+    else:
+        raise ValueError(f"Invalid interval format: {interval_str}")
+
+
+def load_subscribers():
+    global subscribed_chats
+    if os.path.exists(SUBSCRIBERS_FILE):
+        with open(SUBSCRIBERS_FILE, "r") as f:
+            subscribed_chats = json.load(f)
+            for chat_id in subscribed_chats:
+                subscribed_chats[chat_id] = parse_interval(subscribed_chats[chat_id])
+
+def save_subscribers():
+    with open(SUBSCRIBERS_FILE, "w") as f:
+        data = {chat_id: str(interval) for chat_id, interval in subscribed_chats.items()}
+        json.dump(data, f)
+
 def start(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
     interval_str = context.args[0] if context.args else "1m"
     interval = parse_interval(interval_str)
     subscribed_chats[chat_id] = interval
+    save_subscribers()  # Сохраняем подписчиков при подписке
     update.message.reply_text(f'Вы подписались на рассылку видео с интервалом {interval_str}.')
 
+def stop(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    if chat_id in subscribed_chats:
+        del subscribed_chats[chat_id]
+        save_subscribers()
+        update.message.reply_text('Вы успешно отписались от рассылки видео.')
+    else:
+        update.message.reply_text('Вы не подписаны на рассылку видео.')
+
 def main():
+    load_subscribers()
     updater = Updater(TELEGRAM_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
@@ -103,7 +144,7 @@ def main():
     interval = timedelta(minutes=1)
     job_queue.run_repeating(schedule_random_video, interval=interval)
     dispatcher.add_handler(CommandHandler("video", send_video))
-
+    dispatcher.add_handler(CommandHandler("stop", stop))
     updater.start_polling()
     updater.idle()
 
